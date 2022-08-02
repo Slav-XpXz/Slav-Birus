0b2d3ca67388a7ad1867e7265bc0d60d34d22fd7
Ethan Zaznap <Nightmareboi000@gmail.com>
2022-08-01 22:04:57 -0400

    first commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+
+    fp = fopen("./website_files/src/index.html", "r");
+
+    return 0;
+
+}
